56cae286cf08e553150fefbcbe78653c879de857
programmingArrow <nishant.kashyap2104@gmail.com>
2019-11-28 23:18:10 +0530

    Added Navbar Component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+import Navbar from './components/layout/Navbar';
+// import Index from './components/layout/Index';
 
 function App() {
   return (
-    <React.Fragment>
-      Hello World
-    </React.Fragment>
+    <Router>
+      <React.Fragment>
+        <Navbar />
+        <div className="container">
+          <Switch>
+            <Route exact path="/" component="Index" />
+          </Switch>
+        </div>
+      </React.Fragment>
+    </Router>
   );
 }
 
