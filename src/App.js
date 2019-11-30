c4bbb2816940b1caf1e3c2febaa64f1d239fd26e
programmingArrow <nishant.kashyap2104@gmail.com>
2019-11-30 23:20:57 +0530

    Added context to project

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
+import { Provider } from './context';
+
 import Navbar from './components/layout/Navbar';
 // import Index from './components/layout/Index';
 
 function App() {
   return (
-    <Router>
-      <React.Fragment>
-        <Navbar />
-        <div className="container">
-          <Switch>
-            <Route exact path="/" component="Index" />
-          </Switch>
-        </div>
-      </React.Fragment>
-    </Router>
+    <Provider>
+      <Router>
+        <React.Fragment>
+          <Navbar />
+          <div className="container">
+            <Switch>
+              <Route exact path="/" component="Index" />
+            </Switch>
+          </div>
+        </React.Fragment>
+      </Router>
+    </Provider>
   );
 }
 
