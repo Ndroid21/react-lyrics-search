6e6518b4177d9ba59e890148de2fbcd92a0a640f
programmingArrow <nishant.kashyap2104@gmail.com>
2019-12-07 14:39:23 +0530

    Added Search component to Index Component

--- src/components/layout/Index.js
+++ src/components/layout/Index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
 import TrackList from '../tracks/TrackList';
+import Search from './Search';
 
 export default function Index() {
     return (
         <div>
+            <Search />
             <TrackList />
         </div>
     )
